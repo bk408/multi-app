242a4214d4f02b6f99390b4d80df5e91855a2f8f
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2024-04-11 00:07:18 +0530

    SetUp the store

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Login from './components/Login'
 import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 function App() {
   
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <Provider store={store} >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App
